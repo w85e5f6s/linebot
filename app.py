7b3942531484111b06e60a77ba770276a07db54a
w85e5f6s <99731599+w85e5f6s@users.noreply.github.com>
2023-05-16 15:43:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,10 +23,31 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    input_text = event.message.text
+    if input_text == "查匯率":
+        resp = requests.get('https://tw.rter.info/capi.php')
+        currency_data = resp.json()
+        usd_to_twd = currency_data['USDTWD']['Exrate']
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=f'美元 USD 對台幣 TWD：1:{usd_to_twd}'))
+    elif input_text == '現在時間':
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=f"台北時間：{current_time}"))
+    elif input_text == "現在幾度" elif input_text == "現在幾度" or input_text == "溫度":
+        Rdata = requests.get(
+            'https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=ABC-#@12345324-KEY-1233-1237485723&locationName=%E8%87%BA%E5%8C%97,&elementName=TEMP&parameterName=CITY').json()
+        Temp = Rdata["records"]["location"][0]["weatherElement"]
+        Mdict = dict(enumerate(Temp))
+        o1 = Mdict[0]['elementValue']
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=f'現在「台北」的目前溫度是：{o1}'))
+    elif input_text == "股票":
 
-import os
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+        stock(x)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=f'現在「股票號碼{stock} 」的目前股價是：{getstock}'))
